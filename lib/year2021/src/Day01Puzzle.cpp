441010c2ab2716b17c3affbe0473970eb8da2f91
Mark Davison <markdavison0+github@gmail.com>
2021-12-01 18:56:58 +1300

    2021 day 1

--- lib/year2021/src/Day01Puzzle.cpp
+++ lib/year2021/src/Day01Puzzle.cpp
@@ -4,7 +4,7 @@
 namespace TwentyTwentyOne {
 	
 	Day01Puzzle::Day01Puzzle() :
-		core::PuzzleBase("Untitled Puzzle", 2021, 1) {
+		core::PuzzleBase("Sonar Sweep", 2021, 1) {
 	}
 
 
@@ -17,6 +17,31 @@ namespace TwentyTwentyOne {
 	}
 
 	std::pair<std::string, std::string> Day01Puzzle::fastSolve() {
-		return { "Part 1", "Part 2" };
+		auto depths = std::vector<int>();
+
+		for (const auto& s : m_InputLines) {
+			depths.push_back(std::stoi(s));
+		}
+
+		int currentDepth = depths.front();
+		int part1 = 0;
+		for (unsigned i = 1; i < depths.size(); ++i) {
+			if (depths[i] > currentDepth) {
+				part1++;
+			}
+			currentDepth = depths[i];
+		}
+
+		int part2 = 0;
+		int currentDepth2 = depths[0] + depths[1] + depths[2];
+
+		for (unsigned i = 1; i + 2u < depths.size(); ++i) {
+			if (depths[i + 0u] + depths[i + 1u] + depths[i + 2u] > currentDepth2) {
+				part2++;
+			}
+			currentDepth2 = depths[i + 0u] + depths[i + 1u] + depths[i + 2u];
+		}
+
+		return { std::to_string(part1), std::to_string(part2) };
 	}
 }
--- test/2021/Day01PuzzleTests.cpp
+++ test/2021/Day01PuzzleTests.cpp
@@ -4,23 +4,27 @@
 namespace TwentyTwentyOne {
 
 	TEST_CASE("Day 1 Part 1 Example work", "[2021][Day01]") {
-	    const std::vector<std::string> input = {};
+	    const std::vector<std::string> input = {
+"199",
+"200",
+"208",
+"210",
+"200",
+"207",
+"240",
+"269",
+"260",
+"263",
+		};
 
 	    Day01Puzzle puzzle{};
 	    puzzle.setVerbose(true);
 	    puzzle.setInputLines(input);
 
 	    auto answers = puzzle.fastSolve();
-	}
 
-	TEST_CASE("Day 1 Part 2 Example work", "[2021][Day01]") {
-	    const std::vector<std::string> input = {};
-
-	    Day01Puzzle puzzle{};
-	    puzzle.setVerbose(true);
-	    puzzle.setInputLines(input);
-
-	    auto answers = puzzle.fastSolve();
+		REQUIRE("7" == answers.first);
+		REQUIRE("5" == answers.second);
 	}
 
 }
