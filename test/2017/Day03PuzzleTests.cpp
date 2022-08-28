0889089dcbbe94b7b8624cd015c979db863e95b7
mark.davison <mark.davison@taxlab.online>
2022-08-28 18:57:14 +1200

    add more tests for 2017 day 3 part 2

--- test/2017/Day03PuzzleTests.cpp
+++ test/2017/Day03PuzzleTests.cpp
@@ -3,7 +3,7 @@
 
 namespace TwentySeventeen {
 
-	void expectTest(int _input, int _expected) {
+	void expectTest(int _input, int _expected1, int _expected2) {
 		const std::vector<std::string> input = {
 			std::to_string(_input)
 		};
@@ -13,17 +13,18 @@ namespace TwentySeventeen {
 		puzzle.setInputLines(input);
 
 		auto answers = puzzle.fastSolve();
-		REQUIRE(std::to_string(_expected) == answers.first);
+		REQUIRE(std::to_string(_expected1) == answers.first);
+		REQUIRE(std::to_string(_expected2) == answers.second);
 	}
 
 	TEST_CASE("2017 Day 3 Examples work", "[2017][Day03]") {
-		expectTest(26, 5);
-		expectTest(12, 3);
-		expectTest(16, 3);
-		expectTest(20, 3);
-		expectTest(23, 2);
-		expectTest(24, 3);
-		expectTest(1024, 31);
+		expectTest(26, 5, 54);
+		expectTest(12, 3, 23);
+		expectTest(16, 3, 23);
+		expectTest(20, 3, 23);
+		expectTest(23, 2, 25);
+		expectTest(24, 3, 25);
+		expectTest(1024, 31, 1968);
 	}
 
 }
