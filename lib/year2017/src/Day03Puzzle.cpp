492a1789af48e6247384ec9053d3d7d0cc0d3c72
mark.davison <mark.davison@taxlab.online>
2022-08-28 18:51:42 +1200

    2017 day 3

--- lib/year2017/src/Day03Puzzle.cpp
+++ lib/year2017/src/Day03Puzzle.cpp
@@ -1,10 +1,12 @@
 #include <2017/Day03Puzzle.hpp>
 #include <zeno-engine/Utility/StringExtensions.hpp>
+#include <Core/Region.hpp>
+#include <zeno-engine/Core/Vector2.hpp>
 
 namespace TwentySeventeen {
 	
 	Day03Puzzle::Day03Puzzle() :
-		core::PuzzleBase("Untitled Puzzle", 2017, 3) {
+		core::PuzzleBase("Spiral Memory", 2017, 3) {
 
 	}
 	Day03Puzzle::~Day03Puzzle() {
@@ -20,7 +22,144 @@ namespace TwentySeventeen {
 		m_InputLines = std::vector<std::string>(_inputLines);
 	}
 
+	int sumExistingNeighbours(core::Region<int>& _region, const ze::Vector2i& _loc) {
+		int sum = 0;
+
+		sum += _region.getCell(_loc.x - 1, _loc.y - 1);
+		sum += _region.getCell(_loc.x + 0, _loc.y - 1);
+		sum += _region.getCell(_loc.x + 1, _loc.y - 1);
+
+		sum += _region.getCell(_loc.x - 1, _loc.y + 0);
+		sum += _region.getCell(_loc.x + 0, _loc.y + 0);
+		sum += _region.getCell(_loc.x + 1, _loc.y + 0);
+
+		sum += _region.getCell(_loc.x - 1, _loc.y + 1);
+		sum += _region.getCell(_loc.x + 0, _loc.y + 1);
+		sum += _region.getCell(_loc.x + 1, _loc.y + 1);
+
+		return sum;
+	}
+
 	std::pair<std::string, std::string> Day03Puzzle::fastSolve() {
-		return { "Part 1", "Part 2" };
+		const int input = std::stoi(m_InputLines[0]);
+
+		int part1 = 0;
+		
+		int i;
+		for (i = 1; ; i += 2) {
+			if (i * i >= input) {
+				const auto ring = (i - 2) / 2;
+				const auto known = (i - 2) * (i - 2);
+				auto current = known;
+
+				ze::Vector2i loc{ (i - 2) / 2, (i - 2) / 2 };
+
+				current += 1;
+				loc.x += 1;
+
+				if (current == input) {
+					part1 = std::abs(loc.x) + std::abs(loc.y);
+				}
+
+				const auto ringSize = (ring + 1) * 2;
+
+				// moving up
+				for (int j = 0; j < ringSize - 1; ++j) {
+					current += 1;
+					loc.y -= 1;
+
+					if (current == input) {
+						part1 = std::abs(loc.x) + std::abs(loc.y);
+						break;
+					}
+				}
+
+				// moving left
+				for (int j = 0; j < ringSize; ++j) {
+					current += 1;
+					loc.x -= 1;
+
+					if (current == input) {
+						part1 = std::abs(loc.x) + std::abs(loc.y);
+						break;
+					}
+				}
+
+				// moving down
+				for (int j = 0; j < ringSize; ++j) {
+					current += 1;
+					loc.y += 1;
+
+					if (current == input) {
+						part1 = std::abs(loc.x) + std::abs(loc.y);
+						break;
+					}
+				}
+
+				// moving right
+				for (int j = 0; j < ringSize; ++j) {
+					current += 1;
+					loc.x += 1;
+
+					if (current == input) {
+						part1 = std::abs(loc.x) + std::abs(loc.y);
+						break;
+					}
+				}
+
+				break;
+			}
+		}
+
+		core::Region<int> board;
+
+		board.getCell(0, 0) = 1;
+
+		int ringSize = 1;
+		ze::Vector2i loc{ 1,-1 };
+		while (true) {
+			// Moving UP
+			for (int i = 0; i <= ringSize; ++i) {
+				loc.y += 1;
+				auto newVal = sumExistingNeighbours(board, loc);
+				board.getCell(loc.x, loc.y) = newVal;
+				if (newVal > input) {
+					return { std::to_string(part1), std::to_string(newVal) };
+				}
+			}
+
+			// Moving LEFT
+			for (int i = 0; i <= ringSize; ++i) {
+				loc.x -= 1;
+				auto newVal = sumExistingNeighbours(board, loc);
+				board.getCell(loc.x, loc.y) = newVal;
+				if (newVal > input) {
+					return { std::to_string(part1), std::to_string(newVal) };
+				}
+			}
+
+			// Moving DOWN
+			for (int i = 0; i <= ringSize; ++i) {
+				loc.y -= 1;
+				auto newVal = sumExistingNeighbours(board, loc);
+				board.getCell(loc.x, loc.y) = newVal;
+				if (newVal > input) {
+					return { std::to_string(part1), std::to_string(newVal) };
+				}
+			}
+
+			// Moving RIGHT
+			for (int i = 0; i <= ringSize; ++i) {
+				loc.x += 1;
+				auto newVal = sumExistingNeighbours(board, loc);
+				board.getCell(loc.x, loc.y) = newVal;
+				if (newVal > input) {
+					return { std::to_string(part1), std::to_string(newVal) };
+				}
+			}
+
+			ringSize += 2;
+			loc += { 1, -1 };
+		}
 	}
 }
--- test/2017/Day03PuzzleTests.cpp
+++ test/2017/Day03PuzzleTests.cpp
@@ -3,24 +3,27 @@
 
 namespace TwentySeventeen {
 
-	TEST_CASE("2017 Day 3 Part 1 Example work", "[2017][Day03]") {
-	    const std::vector<std::string> input = {};
+	void expectTest(int _input, int _expected) {
+		const std::vector<std::string> input = {
+			std::to_string(_input)
+		};
 
-	    Day03Puzzle puzzle{};
-	    puzzle.setVerbose(true);
-	    puzzle.setInputLines(input);
+		Day03Puzzle puzzle{};
+		puzzle.setVerbose(true);
+		puzzle.setInputLines(input);
 
-	    auto answers = puzzle.fastSolve();
+		auto answers = puzzle.fastSolve();
+		REQUIRE(std::to_string(_expected) == answers.first);
 	}
 
-	TEST_CASE("2017 Day 3 Part 2 Example work", "[2017][Day03]") {
-	    const std::vector<std::string> input = {};
-
-	    Day03Puzzle puzzle{};
-	    puzzle.setVerbose(true);
-	    puzzle.setInputLines(input);
-
-	    auto answers = puzzle.fastSolve();
+	TEST_CASE("2017 Day 3 Examples work", "[2017][Day03]") {
+		expectTest(26, 5);
+		expectTest(12, 3);
+		expectTest(16, 3);
+		expectTest(20, 3);
+		expectTest(23, 2);
+		expectTest(24, 3);
+		expectTest(1024, 31);
 	}
 
 }
