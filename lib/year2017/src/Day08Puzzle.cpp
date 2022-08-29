6607357eef35ddd9f46bc3857ee2afdc0e45c8b9
mark.davison <mark.davison@taxlab.online>
2022-08-29 14:34:23 +1200

    2017 day 8

--- lib/year2017/src/Day08Puzzle.cpp
+++ lib/year2017/src/Day08Puzzle.cpp
@@ -1,10 +1,14 @@
 #include <2017/Day08Puzzle.hpp>
 #include <zeno-engine/Utility/StringExtensions.hpp>
+#include <unordered_map>
 
 namespace TwentySeventeen {
-	
+
+	using RegisterValue = long;
+	using Registers = std::unordered_map<std::string, RegisterValue>;
+
 	Day08Puzzle::Day08Puzzle() :
-		core::PuzzleBase("Untitled Puzzle", 2017, 8) {
+		core::PuzzleBase("I Heard You Like Registers", 2017, 8) {
 
 	}
 	Day08Puzzle::~Day08Puzzle() {
@@ -21,6 +25,58 @@ namespace TwentySeventeen {
 	}
 
 	std::pair<std::string, std::string> Day08Puzzle::fastSolve() {
-		return { "Part 1", "Part 2" };
+		Registers registers;
+
+		RegisterValue part1 = 0;
+		RegisterValue part2 = 0;
+
+		for (const auto& l : m_InputLines) {
+			const auto& parts = ze::StringExtensions::splitStringByDelimeter(l, " ");
+			const auto destRegisterName = parts[0];
+			const auto change = ((RegisterValue)std::stol(parts[2])) * (parts[1] == "inc" ? +1 : -1);
+
+			const auto val1 = parts[4];
+			const auto val2 = parts[6];
+
+			const auto number1 = ('a' <= val1[0] && val1[0] <= 'z') ? registers[val1] : (RegisterValue)std::stol(val1);
+			const auto number2 = ('a' <= val2[0] && val2[0] <= 'z') ? registers[val2] : (RegisterValue)std::stol(val2);
+
+			bool result = false;
+
+			const auto op = parts[5];
+
+			if (op == ">") {
+				result = number1 > number2;
+			}
+			else if (op == "<") {
+				result = number1 < number2;
+			}
+			else if (op == ">=") {
+				result = number1 >= number2;
+			}
+			else if (op == "<=") {
+				result = number1 <= number2;
+			}
+			else if (op == "==") {
+				result = number1 == number2;
+			}
+			else if (op == "!=") {
+				result = number1 != number2;
+			}
+			else {
+				throw std::runtime_error("Unhandled operation");
+			}
+
+			if (result) {
+				registers[destRegisterName] += change;
+				part2 = std::max(registers[destRegisterName], part2);
+			}
+		}
+
+		for (const auto& [name, value] : registers) {
+			part1 = std::max(part1, value);
+		}
+
+		return { std::to_string(part1), std::to_string(part2) };
 	}
 }
--- test/2017/Day08PuzzleTests.cpp
+++ test/2017/Day08PuzzleTests.cpp
@@ -4,23 +4,21 @@
 namespace TwentySeventeen {
 
 	TEST_CASE("2017 Day 8 Part 1 Example work", "[2017][Day08]") {
-	    const std::vector<std::string> input = {};
+	    const std::vector<std::string> input = {
+"b inc 5 if a > 1",
+"a inc 1 if b < 5",
+"c dec -10 if a >= 1",
+"c inc -20 if c == 10"
+		};
 
 	    Day08Puzzle puzzle{};
 	    puzzle.setVerbose(true);
 	    puzzle.setInputLines(input);
 
 	    auto answers = puzzle.fastSolve();
-	}
 
-	TEST_CASE("2017 Day 8 Part 2 Example work", "[2017][Day08]") {
-	    const std::vector<std::string> input = {};
-
-	    Day08Puzzle puzzle{};
-	    puzzle.setVerbose(true);
-	    puzzle.setInputLines(input);
-
-	    auto answers = puzzle.fastSolve();
+		REQUIRE("1" == answers.first);
+		REQUIRE("10" == answers.second);
 	}
 
 }
