57b6ae8057e3a4015afd6ece6c9273692a3d83aa
mark.davison <mark.davison@taxlab.online>
2022-09-01 16:29:04 +1200

    2017 day 13

--- lib/year2017/src/Day13Puzzle.cpp
+++ lib/year2017/src/Day13Puzzle.cpp
@@ -1,10 +1,11 @@
 #include <2017/Day13Puzzle.hpp>
 #include <zeno-engine/Utility/StringExtensions.hpp>
+#include <unordered_map>
 
 namespace TwentySeventeen {
 	
 	Day13Puzzle::Day13Puzzle() :
-		core::PuzzleBase("Untitled Puzzle", 2017, 13) {
+		core::PuzzleBase("Packet Scanners", 2017, 13) {
 
 	}
 	Day13Puzzle::~Day13Puzzle() {
@@ -21,6 +22,35 @@ namespace TwentySeventeen {
 	}
 
 	std::pair<std::string, std::string> Day13Puzzle::fastSolve() {
-		return { "Part 1", "Part 2" };
+
+		std::unordered_map<std::size_t, std::size_t> firewall;
+		for (const auto& l : m_InputLines) {
+			const auto& p = ze::StringExtensions::splitStringByDelimeter(l, " :");
+			firewall[(std::size_t)std::stoul(p[0])] = (std::size_t)std::stoul(p[1]);
+		}
+
+		std::size_t severity = 0;
+
+		for (const auto& [layer, depth] : firewall) {
+			if (layer % ((depth - 1) * 2) == 0) {
+				severity += layer * depth;
+			}
+		}
+
+		std::size_t delay;
+		for (delay = 0; ; ++delay) {
+			bool valid = true;
+			for (const auto& [layer, depth] : firewall) {
+				if ((layer + delay) % ((depth - 1) * 2) == 0) {
+					valid = false;
+					break;
+				}
+			}
+			if (valid) {
+				break;
+			}
+		}
+
+		return { std::to_string(severity), std::to_string(delay) };
 	}
 }
--- test/2017/Day13PuzzleTests.cpp
+++ test/2017/Day13PuzzleTests.cpp
@@ -3,24 +3,22 @@
 
 namespace TwentySeventeen {
 
-	TEST_CASE("2017 Day 13 Part 1 Example work", "[2017][Day13]") {
-	    const std::vector<std::string> input = {};
+	TEST_CASE("2017 Day 13 Examples work", "[2017][Day13]") {
+	    const std::vector<std::string> input = {
+"0: 3",
+"1: 2",
+"4: 4",
+"6: 4"
+		};
 
 	    Day13Puzzle puzzle{};
 	    puzzle.setVerbose(true);
 	    puzzle.setInputLines(input);
 
 	    auto answers = puzzle.fastSolve();
-	}
 
-	TEST_CASE("2017 Day 13 Part 2 Example work", "[2017][Day13]") {
-	    const std::vector<std::string> input = {};
-
-	    Day13Puzzle puzzle{};
-	    puzzle.setVerbose(true);
-	    puzzle.setInputLines(input);
-
-	    auto answers = puzzle.fastSolve();
+		REQUIRE("24" == answers.first);
+		REQUIRE("10" == answers.second);
 	}
 
 }
