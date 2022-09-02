3c20e63b4ba7a565337a38727231ad6c341a691b
mark.davison <mark.davison@taxlab.online>
2022-09-02 15:54:53 +1200

    2017 day 17

--- lib/year2017/src/Day17Puzzle.cpp
+++ lib/year2017/src/Day17Puzzle.cpp
@@ -4,7 +4,7 @@
 namespace TwentySeventeen {
 	
 	Day17Puzzle::Day17Puzzle() :
-		core::PuzzleBase("Untitled Puzzle", 2017, 17) {
+		core::PuzzleBase("Spinlock", 2017, 17) {
 
 	}
 	Day17Puzzle::~Day17Puzzle() {
@@ -20,7 +20,54 @@ namespace TwentySeventeen {
 		m_InputLines = std::vector<std::string>(_inputLines);
 	}
 
+	struct Node {
+		Node* left{ nullptr };
+		Node* right{ nullptr };
+		int value;
+	};
+
 	std::pair<std::string, std::string> Day17Puzzle::fastSolve() {
-		return { "Part 1", "Part 2" };
+		const int step = std::stoi(m_InputLines[0]);
+
+		Node* root = new Node();
+
+		root->left = root;
+		root->right = root;
+		root->value = 0;
+
+		Node* curr = root;
+		int currentValue = 0;
+
+		for (std::size_t iter = 0; iter < 2017; ++iter) {
+			for (int i = 0; i < step; ++i) {
+				curr = curr->right;
+			}
+
+			Node* next = curr->right;
+
+			Node* newNode = new Node();
+			newNode->value = ++currentValue;
+
+			curr->right = newNode;
+			newNode->left = curr;
+
+			newNode->right = next;
+			next->left = next;
+
+			curr = newNode;
+		}
+
+		const auto part1 = curr->right->value;
+
+		int i = 0;
+		int part2 = 0;
+		for (int t = 1; t < 50000000 + 1; ++t) {
+			i = (i + step) % t + 1;
+			if (i == 1) {
+				part2 = t;
+			}
+		}
+
+		return { std::to_string(part1), std::to_string(part2) };
 	}
 }
--- test/2017/Day17PuzzleTests.cpp
+++ test/2017/Day17PuzzleTests.cpp
@@ -3,24 +3,16 @@
 
 namespace TwentySeventeen {
 
-	TEST_CASE("2017 Day 17 Part 1 Example work", "[2017][Day17]") {
-	    const std::vector<std::string> input = {};
+	TEST_CASE("2017 Day 17 Examples work", "[2017][Day17]") {
+	    const std::vector<std::string> input = { "3" };
 
 	    Day17Puzzle puzzle{};
 	    puzzle.setVerbose(true);
 	    puzzle.setInputLines(input);
 
 	    auto answers = puzzle.fastSolve();
-	}
 
-	TEST_CASE("2017 Day 17 Part 2 Example work", "[2017][Day17]") {
-	    const std::vector<std::string> input = {};
-
-	    Day17Puzzle puzzle{};
-	    puzzle.setVerbose(true);
-	    puzzle.setInputLines(input);
-
-	    auto answers = puzzle.fastSolve();
+		REQUIRE("638" == answers.first);
 	}
 
 }
