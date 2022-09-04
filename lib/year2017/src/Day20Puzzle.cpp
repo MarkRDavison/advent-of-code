e63ec3b5e8ef355c308805d0c594302bf3ea9972
mark.davison <mark.davison@taxlab.online>
2022-09-04 15:40:13 +1200

    2017 DAY 20

--- lib/year2017/src/Day20Puzzle.cpp
+++ lib/year2017/src/Day20Puzzle.cpp
@@ -1,10 +1,20 @@
 #include <2017/Day20Puzzle.hpp>
 #include <zeno-engine/Utility/StringExtensions.hpp>
+#include <zeno-engine/Core/Vector3.hpp>
+#include <unordered_map>
+#include <algorithm>
 
 namespace TwentySeventeen {
+
+	template<typename T>
+	struct vector3_hash_fxn {
+		std::size_t operator()(const ze::Vector3<T>& _vec) const {
+			return  std::hash<T>()(_vec.x) ^ std::hash<T>()(_vec.y) ^ std::hash<T>()(_vec.z);
+		}
+	};
 	
 	Day20Puzzle::Day20Puzzle() :
-		core::PuzzleBase("Untitled Puzzle", 2017, 20) {
+		core::PuzzleBase("Particle Swarm", 2017, 20) {
 
 	}
 	Day20Puzzle::~Day20Puzzle() {
@@ -20,7 +30,61 @@ namespace TwentySeventeen {
 		m_InputLines = std::vector<std::string>(_inputLines);
 	}
 
+	struct Particle {
+		ze::Vector3i p;
+		ze::Vector3i v;
+		ze::Vector3i a;
+	};
+
 	std::pair<std::string, std::string> Day20Puzzle::fastSolve() {
-		return { "Part 1", "Part 2" };
+		std::vector<Particle> particles;
+
+		for (const auto& l : m_InputLines) {
+			const auto& parts = ze::StringExtensions::splitStringByDelimeter(l, "=<>,");
+
+			auto& particle = particles.emplace_back();
+
+			particle.p = ze::Vector3i(std::stoi(parts[1]), std::stoi(parts[2]), std::stoi(parts[3]));
+			particle.v = ze::Vector3i(std::stoi(parts[5]), std::stoi(parts[6]), std::stoi(parts[7]));
+			particle.a = ze::Vector3i(std::stoi(parts[9]), std::stoi(parts[10]), std::stoi(parts[11]));
+		}
+
+		std::size_t part1 = 0;
+		std::size_t i = 0;
+		int minAccMagnitude = std::numeric_limits<int>::max();
+
+		for (const auto& p : particles) {
+			const int accMagnitude = std::abs(p.a.x) + std::abs(p.a.y) + std::abs(p.a.z);
+			if (minAccMagnitude > accMagnitude) {
+				minAccMagnitude = accMagnitude;
+				part1 = i;
+			}
+			i++;
+		}
+
+		std::size_t collisionThreshold = 10;
+		i = 0;
+		while (i < collisionThreshold) {
+			std::unordered_map<ze::Vector3i, int, vector3_hash_fxn<int>> positions;
+			for (auto& p : particles) {
+				p.v += p.a;
+				p.p += p.v;
+
+				positions[p.p]++;
+			}
+
+			if (positions.size() != particles.size()) {
+				i = 0;
+				particles.erase(std::remove_if(particles.begin(), particles.end(),
+					[&positions](const Particle& _p) -> bool {
+						return positions[_p.p] > 1;
+					}), particles.end());
+			}
+			else {
+				++i;
+			}
+		}
+
+		return { std::to_string(part1), std::to_string(particles.size()) };
 	}
 }
--- test/2017/Day20PuzzleTests.cpp
+++ test/2017/Day20PuzzleTests.cpp
@@ -3,24 +3,22 @@
 
 namespace TwentySeventeen {
 
-	TEST_CASE("2017 Day 20 Part 1 Example work", "[2017][Day20]") {
-	    const std::vector<std::string> input = {};
+	TEST_CASE("2017 Day 20 Examples work", "[2017][Day20]") {
+	    const std::vector<std::string> input = {
+"p=<-6,0,0>, v=< 3,0,0>, a=< 0,0,0>",
+"p=<-4,0,0>, v=< 2,0,0>, a=< 0,0,0>",
+"p=<-2,0,0>, v=< 1,0,0>, a=< 0,0,0>",
+"p=< 3,0,0>, v=<-1,0,0>, a=< 0,0,0>"
+		};
 
 	    Day20Puzzle puzzle{};
 	    puzzle.setVerbose(true);
 	    puzzle.setInputLines(input);
 
 	    auto answers = puzzle.fastSolve();
-	}
 
-	TEST_CASE("2017 Day 20 Part 2 Example work", "[2017][Day20]") {
-	    const std::vector<std::string> input = {};
-
-	    Day20Puzzle puzzle{};
-	    puzzle.setVerbose(true);
-	    puzzle.setInputLines(input);
-
-	    auto answers = puzzle.fastSolve();
+		REQUIRE("0" == answers.first);
+		REQUIRE("1" == answers.second);
 	}
 
 }
