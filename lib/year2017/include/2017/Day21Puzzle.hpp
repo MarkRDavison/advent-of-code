b00a39247df2d7761e641fdaeaca9a035bef88bc
mark.davison <mark.davison@taxlab.online>
2022-09-06 19:13:34 +1200

    2017 day 21

--- lib/year2017/include/2017/Day21Puzzle.hpp
+++ lib/year2017/include/2017/Day21Puzzle.hpp
@@ -2,9 +2,32 @@
 #define INCLUDED_ADVENT_OF_CODE_2017_DAY_21_PUZZLE_HPP_
 
 #include <Core/PuzzleBase.hpp>
+#include <Core/Region.hpp>
+#include <unordered_map>
 
 namespace TwentySeventeen {
-	
+
+	struct Grid : std::vector<std::string> {
+
+		std::string hash() const {
+			std::string h;
+
+			for (std::size_t i = 0; i < this->size(); ++i) {
+				h += this->at(i) + "/";
+			}
+
+			return h.substr(0, h.size() - 1);
+		}
+	};
+
+	struct Recipe {
+		Grid src;
+		Grid dst;
+	};
+
+	using RecipeBook = std::unordered_map<std::string, std::string>;
+	using Art = core::Region<char>;
+
 	class Day21Puzzle : public core::PuzzleBase {
 	public:
 		Day21Puzzle();
@@ -13,6 +36,15 @@ namespace TwentySeventeen {
 		void initialise(const core::InitialisationInfo& _initialisationInfo) override;
 		void setInputLines(const std::vector<std::string>& _inputLines);
 		std::pair<std::string, std::string> fastSolve() override;
+		std::string fastSolve(std::size_t _iter);
+		void printArt(Art& _art);
+		Art solveForIter(std::size_t _iter);
+
+		static Grid flip(const Grid& _grid);
+		static Grid mirror(const Grid& _grid);
+		static Grid rotate(const Grid& _grid);
+		static Grid rotate(const Grid& _grid, std::size_t _iters);
+
 
 	private:
 		std::vector<std::string> m_InputLines;
--- test/2017/Day21PuzzleTests.cpp
+++ test/2017/Day21PuzzleTests.cpp
@@ -3,24 +3,161 @@
 
 namespace TwentySeventeen {
 
-	TEST_CASE("2017 Day 21 Part 1 Example work", "[2017][Day21]") {
-	    const std::vector<std::string> input = {};
+	TEST_CASE("2017 Day 21 Examples work", "[2017][Day21]") {
+	    const std::vector<std::string> input = {
+"../.# => ##./#../...",
+".#./..#/### => #..#/..../..../#..#"
+		};
 
 	    Day21Puzzle puzzle{};
 	    puzzle.setVerbose(true);
 	    puzzle.setInputLines(input);
 
-	    auto answers = puzzle.fastSolve();
+		REQUIRE("12" == puzzle.fastSolve(2));
 	}
 
-	TEST_CASE("2017 Day 21 Part 2 Example work", "[2017][Day21]") {
-	    const std::vector<std::string> input = {};
+	TEST_CASE("2017 Day 21 mirror works", "[2017][Day21]") {
+		SECTION("2x2 works") {
+			Grid grid = (Grid)std::vector<std::string>{
+				"ab",
+				"cd"
+			};
 
-	    Day21Puzzle puzzle{};
-	    puzzle.setVerbose(true);
-	    puzzle.setInputLines(input);
+			const auto& mirrored = Day21Puzzle::mirror(grid);
+			REQUIRE("ba" == mirrored[0]);
+			REQUIRE("dc" == mirrored[1]);
+		}
+		SECTION("3x3 works") {
+			Grid grid = (Grid)std::vector<std::string>{
+				"abc",
+				"def",
+				"ghi"
+			};
 
-	    auto answers = puzzle.fastSolve();
+			const auto& mirrored = Day21Puzzle::mirror(grid);
+			REQUIRE("cba" == mirrored[0]);
+			REQUIRE("fed" == mirrored[1]);
+			REQUIRE("ihg" == mirrored[2]);
+		}
 	}
+	TEST_CASE("2017 Day 21 flip works", "[2017][Day21]") {
+		SECTION("2x2 works") {
+			Grid grid = (Grid)std::vector<std::string>{
+				"ab",
+				"cd"
+			};
 
+			const auto& flipped = Day21Puzzle::flip(grid);
+			REQUIRE("cd" == flipped[0]);
+			REQUIRE("ab" == flipped[1]);
+		}
+		SECTION("3x3 works") {
+			Grid grid = (Grid)std::vector<std::string>{
+				"abc",
+				"def",
+				"ghi"
+			};
+
+			const auto& flipped = Day21Puzzle::flip(grid);
+			REQUIRE("ghi" == flipped[0]);
+			REQUIRE("def" == flipped[1]);
+			REQUIRE("abc" == flipped[2]);
+		}
+	}
+	TEST_CASE("2017 Day 21 rotate works", "[2017][Day21]") {
+		SECTION("2x2 works") {
+			Grid grid = (Grid)std::vector<std::string>{
+				"ab",
+				"cd"
+			};
+
+			auto rotated = Day21Puzzle::rotate(grid);
+
+			REQUIRE("ca" == rotated[0]);
+			REQUIRE("db" == rotated[1]);
+			rotated = Day21Puzzle::rotate(rotated);
+			REQUIRE("dc" == rotated[0]);
+			REQUIRE("ba" == rotated[1]);
+			rotated = Day21Puzzle::rotate(rotated);
+			REQUIRE("bd" == rotated[0]);
+			REQUIRE("ac" == rotated[1]);
+			rotated = Day21Puzzle::rotate(rotated);
+			REQUIRE(grid[0] == rotated[0]);
+			REQUIRE(grid[1] == rotated[1]);
+		}
+		SECTION("2x2 by iter works") {
+			Grid grid = (Grid)std::vector<std::string>{
+				"ab",
+				"cd"
+			};
+
+			auto rotated = Day21Puzzle::rotate(grid, 1);
+
+			REQUIRE("ca" == rotated[0]);
+			REQUIRE("db" == rotated[1]);
+			rotated = Day21Puzzle::rotate(grid, 2);
+			REQUIRE("dc" == rotated[0]);
+			REQUIRE("ba" == rotated[1]);
+			rotated = Day21Puzzle::rotate(grid, 3);
+			REQUIRE("bd" == rotated[0]);
+			REQUIRE("ac" == rotated[1]);
+			rotated = Day21Puzzle::rotate(grid, 4);
+			REQUIRE(grid[0] == rotated[0]);
+			REQUIRE(grid[1] == rotated[1]);
+		}
+		SECTION("3x3 works") {
+			Grid grid = (Grid)std::vector<std::string>{
+				"abc",
+				"def",
+				"ghi"
+			};
+
+			auto rotated = Day21Puzzle::rotate(grid);
+			REQUIRE("gda" == rotated[0]);
+			REQUIRE("heb" == rotated[1]);
+			REQUIRE("ifc" == rotated[2]);
+
+			rotated = Day21Puzzle::rotate(rotated);
+			REQUIRE("ihg" == rotated[0]);
+			REQUIRE("fed" == rotated[1]);
+			REQUIRE("cba" == rotated[2]);
+
+			rotated = Day21Puzzle::rotate(rotated);
+			REQUIRE("cfi" == rotated[0]);
+			REQUIRE("beh" == rotated[1]);
+			REQUIRE("adg" == rotated[2]);
+
+			rotated = Day21Puzzle::rotate(rotated);
+			REQUIRE(grid[0] == rotated[0]);
+			REQUIRE(grid[1] == rotated[1]);
+			REQUIRE(grid[2] == rotated[2]);
+		}
+		SECTION("3x3 iter works") {
+			Grid grid = (Grid)std::vector<std::string>{
+				"abc",
+				"def",
+				"ghi"
+			};
+
+			auto rotated = Day21Puzzle::rotate(grid, 1);
+			REQUIRE("gda" == rotated[0]);
+			REQUIRE("heb" == rotated[1]);
+			REQUIRE("ifc" == rotated[2]);
+
+			rotated = Day21Puzzle::rotate(grid, 2);
+			REQUIRE("ihg" == rotated[0]);
+			REQUIRE("fed" == rotated[1]);
+			REQUIRE("cba" == rotated[2]);
+
+			rotated = Day21Puzzle::rotate(grid, 3);
+			REQUIRE("cfi" == rotated[0]);
+			REQUIRE("beh" == rotated[1]);
+			REQUIRE("adg" == rotated[2]);
+
+			rotated = Day21Puzzle::rotate(grid, 4);
+			REQUIRE(grid[0] == rotated[0]);
+			REQUIRE(grid[1] == rotated[1]);
+			REQUIRE(grid[2] == rotated[2]);
+		}
+	}
 }
