d35b29f69cdeac2e70bf66b6695469787f8c7e99
mark.davison <mark.davison@taxlab.online>
2022-11-22 17:52:18 +1300

    2018 day 5 - bad :(

--- lib/year2018/src/Day05Puzzle.cpp
+++ lib/year2018/src/Day05Puzzle.cpp
@@ -1,10 +1,11 @@
 #include <2018/Day05Puzzle.hpp>
 #include <zeno-engine/Utility/StringExtensions.hpp>
 
+
 namespace TwentyEighteen {
 	
 	Day05Puzzle::Day05Puzzle() :
-		core::PuzzleBase("Untitled Puzzle", 2018, 5) {
+		core::PuzzleBase("Alchemical Reduction", 2018, 5) {
 
 	}
 	Day05Puzzle::~Day05Puzzle() {
@@ -20,7 +21,56 @@ namespace TwentyEighteen {
 		m_InputLines = std::vector<std::string>(_inputLines);
 	}
 
+	void reduce(std::string& _chemical)
+	{
+		for (std::size_t i = 1; i < _chemical.size(); ++i)
+		{
+			const char lhs = _chemical[i - 1];
+			const char rhs = _chemical[i];
+			if (lhs != rhs && std::tolower(lhs) == std::tolower(rhs))
+			{
+				_chemical.erase(i - 1, 2);
+				break;
+			}
+		}
+	}
+
 	std::pair<std::string, std::string> Day05Puzzle::fastSolve() {
-		return { "Part 1", "Part 2" };
+
+		std::size_t part1;
+		std::size_t part2 = std::numeric_limits<std::size_t>::max();
+
+		{
+			std::string chemical(m_InputLines[0]);
+			std::size_t startingSize;
+			do {
+				startingSize = chemical.size();
+				if (startingSize % 100 == 0 || startingSize % 100 == 1)
+				{
+					std::cout << "Part 1 - size: " << startingSize << std::endl;
+				}
+				reduce(chemical);
+			} while (startingSize != chemical.size());
+			part1 = chemical.size();
+		}
+
+		for (int i = 'a'; i <= 'z'; ++i)
+		{
+			std::string chemical(m_InputLines[0]);
+			chemical.erase(std::remove(chemical.begin(), chemical.end(), (char)i), chemical.end());
+			chemical.erase(std::remove(chemical.begin(), chemical.end(), std::toupper((char)i)), chemical.end());
+			std::size_t startingSize;
+			do {
+				startingSize = chemical.size();
+				if (startingSize % 100 == 0 || startingSize % 100 == 1)
+				{
+					std::cout << "Part 2 - testing without " << (char)i << "/" << (char)std::toupper((char)i) << " - size: " << startingSize << std::endl;
+				}
+				reduce(chemical);
+			} while (startingSize != chemical.size());
+			part2 = std::min(part2, chemical.size());
+		}
+
+		return { std::to_string(part1), std::to_string(part2) };
 	}
 }
--- test/2018/Day05PuzzleTests.cpp
+++ test/2018/Day05PuzzleTests.cpp
@@ -3,24 +3,16 @@
 
 namespace TwentyEighteen {
 
-	TEST_CASE("2018 Day 5 Part 1 Example work", "[2018][Day05]") {
-	    const std::vector<std::string> input = {};
+	TEST_CASE("2018 Day 5 Examples work", "[2018][Day05]") {
+	    const std::vector<std::string> input = {"dabAcCaCBAcCcaDA"};
 
 	    Day05Puzzle puzzle{};
 	    puzzle.setVerbose(true);
 	    puzzle.setInputLines(input);
 
 	    auto answers = puzzle.fastSolve();
-	}
-
-	TEST_CASE("2018 Day 5 Part 2 Example work", "[2018][Day05]") {
-	    const std::vector<std::string> input = {};
-
-	    Day05Puzzle puzzle{};
-	    puzzle.setVerbose(true);
-	    puzzle.setInputLines(input);
-
-	    auto answers = puzzle.fastSolve();
+		REQUIRE("10" == answers.first);
+		REQUIRE("4" == answers.second);
 	}
 
 }
