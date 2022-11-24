7f141b9a2af525a47d142261b43852c0732e3f01
mark.davison <mark.davison@taxlab.online>
2022-11-24 15:08:55 +1300

    2018 day 8

--- lib/year2018/src/Day08Puzzle.cpp
+++ lib/year2018/src/Day08Puzzle.cpp
@@ -1,10 +1,14 @@
 #include <2018/Day08Puzzle.hpp>
 #include <zeno-engine/Utility/StringExtensions.hpp>
+#include <algorithm>
+#include <numeric>
+#include <cassert>
+#include <span>
 
 namespace TwentyEighteen {
 	
 	Day08Puzzle::Day08Puzzle() :
-		core::PuzzleBase("Untitled Puzzle", 2018, 8) {
+		core::PuzzleBase("Memory Maneuver", 2018, 8) {
 
 	}
 	Day08Puzzle::~Day08Puzzle() {
@@ -20,7 +24,73 @@ namespace TwentyEighteen {
 		m_InputLines = std::vector<std::string>(_inputLines);
 	}
 
+	void recurse(const std::span<int>& _span, std::size_t& _index, int& _metadataSum, bool _part2)
+	{
+		const auto numChildren = _span[_index + 0];
+		const auto numMetadata = _span[_index + 1];
+
+		_index += 2;
+
+		std::vector<int> childMetadata;
+		for (int i = 0; i < numChildren; ++i)
+		{
+			int childMetadataValue = 0;
+			recurse(_span, _index, childMetadataValue, _part2);
+			childMetadata.push_back(childMetadataValue);
+		}
+
+
+		std::vector<int> selfMetadata;
+		for (int i = 0; i < numMetadata; ++i)
+		{
+			selfMetadata.push_back(_span[_index + i]);
+		}
+
+		if (!_part2)
+		{
+			_metadataSum += std::accumulate(selfMetadata.begin(), selfMetadata.end(), 0);
+			_metadataSum += std::accumulate(childMetadata.begin(), childMetadata.end(), 0);
+		}
+		else
+		{
+			for (const auto& selfMetadataValue : selfMetadata)
+			{
+				if (numChildren == 0)
+				{
+					_metadataSum += selfMetadataValue;
+				}
+				else
+				{
+					std::size_t childMetadataIndex = (std::size_t)selfMetadataValue - 1;
+					if (0 <= childMetadataIndex && childMetadataIndex < childMetadata.size())
+					{
+						_metadataSum += childMetadata[childMetadataIndex];
+					}
+				}
+			}
+		}
+
+		_index += numMetadata;
+	}
+
 	std::pair<std::string, std::string> Day08Puzzle::fastSolve() {
-		return { "Part 1", "Part 2" };
+		std::vector<int> parsed;
+		for (const auto& p : ze::StringExtensions::splitStringByDelimeter(m_InputLines[0], " "))
+		{
+			parsed.push_back(std::stoi(p));
+		}
+
+		int part1 = 0;
+		int part2 = 0;
+		{
+			std::size_t index = 0;
+			recurse(std::span<int>(parsed), index, part1, false);
+		}
+		{
+			std::size_t index = 0;
+			recurse(std::span<int>(parsed), index, part2, true);
+		}
+
+		return { std::to_string(part1), std::to_string(part2) };
 	}
 }
--- test/2018/Day08PuzzleTests.cpp
+++ test/2018/Day08PuzzleTests.cpp
@@ -3,24 +3,17 @@
 
 namespace TwentyEighteen {
 
-	TEST_CASE("2018 Day 8 Part 1 Example work", "[2018][Day08]") {
-	    const std::vector<std::string> input = {};
+	TEST_CASE("2018 Day 8 Examples work", "[2018][Day08]") {
+	    const std::vector<std::string> input = { "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2" };
 
 	    Day08Puzzle puzzle{};
 	    puzzle.setVerbose(true);
 	    puzzle.setInputLines(input);
 
 	    auto answers = puzzle.fastSolve();
-	}
 
-	TEST_CASE("2018 Day 8 Part 2 Example work", "[2018][Day08]") {
-	    const std::vector<std::string> input = {};
-
-	    Day08Puzzle puzzle{};
-	    puzzle.setVerbose(true);
-	    puzzle.setInputLines(input);
-
-	    auto answers = puzzle.fastSolve();
+		REQUIRE("138" == answers.first);
+		REQUIRE("66" == answers.second);
 	}
 
 }
