4eda0bc369dddef4723a9f144552494866c77cb1
mark.davison <mark.davison@taxlab.online>
2022-11-24 14:36:07 +1300

    2018 day 7

--- lib/year2018/src/Day07Puzzle.cpp
+++ lib/year2018/src/Day07Puzzle.cpp
@@ -29,15 +29,89 @@ namespace TwentyEighteen {
 		return fastSolve(60, 5);
 	}
 
+	std::pair<std::string, int> solve(int _extraTime, int _numWorkers, const std::map<char, std::set<char>>& _prereqs)
+	{
+		const auto answerSize = _prereqs.size();
+		int time = -1;
+
+		struct Worker {
+			char assigned{ '.' };
+			int remaining{ 0 };
+		};
+
+		std::vector<Worker> workers(_numWorkers);
+		auto preReqs(_prereqs);
+		std::string done;
+
+		const auto iterateWorker = [&](Worker& _w, bool _busy) -> void
+		{
+			if (_busy)
+			{
+				if (_w.assigned == '.')return;
+			}
+			if (!_busy)
+			{
+				if (_w.assigned != '.')return;
+			}
+
+			if (_w.assigned != '.')
+			{
+				if (_w.remaining == 0)
+				{
+					done += _w.assigned;
+					for (auto& [k, req] : preReqs)
+					{
+						req.erase(_w.assigned);
+					}
+					_w.assigned = '.';
+				}
+			}
+			if (_w.assigned == '.')
+			{
+				char toBeAssigned = '.';
+				for (const auto& [k, req] : preReqs)
+				{
+					if (req.empty())
+					{
+						toBeAssigned = k;
+						break;
+					}
+				}
+
+				if (toBeAssigned != '.')
+				{
+					preReqs.erase(toBeAssigned);
+					_w.assigned = toBeAssigned;
+					_w.remaining = _extraTime + ((int)toBeAssigned - 'A') + 1;
+				}
+			}
+			if (_w.assigned != '.')
+			{
+				_w.remaining--;
+			}
+		};
+		do {
+			time++;
+
+			for (auto& w : workers)
+			{
+				iterateWorker(w, true);
+			}
+			for (auto& w : workers)
+			{
+				iterateWorker(w, false);
+			}
+		} while (done.size() != answerSize);
+
+		return { done, time };
+	}
+
 	std::pair<std::string, std::string> Day07Puzzle::fastSolve(int _extraTime, int _numWorkers)
 	{
 
-		std::unordered_map<char, std::set<char>> rules;
-		std::unordered_map<char, std::set<char>> reverseRules;
+		std::map<char, std::set<char>> rules;
+		std::map<char, std::set<char>> reverseRules;
 		std::set<char> all;
-
-		std::string part1;
-		std::string part2;
 
 		for (const auto& i : m_InputLines)
 		{
@@ -63,111 +137,9 @@ namespace TwentyEighteen {
 			}
 		}
 
-		auto reverseRulesPart2(reverseRules);
+		auto part1 = solve(0, 1, reverseRules);
+		auto part2 = solve(_extraTime, _numWorkers, reverseRules);
 
-		{	//	Part 1
-			std::set<char> availablePart1(available);
-			while (!availablePart1.empty())
-			{
-				const auto front = *availablePart1.begin();
-				availablePart1.erase(front);
-				part1 += front;
-
-
-				for (auto& [k, set] : reverseRules)
-				{
-					if (!set.empty())
-					{
-						set.erase(front);
-						if (set.empty())
-						{
-							availablePart1.insert(k);
-						}
-					}
-				}
-			}
-		}
-
-		int time = 0;
-
-		{	//	Part 2
-			std::string done;
-			std::map<char, bool> availablePart2;
-			for (const auto& k : available)
-			{
-				availablePart2[k] = true;
-			}
-			std::unordered_map<char, int> remainingWork;
-			for (const auto& k : all)
-			{
-				remainingWork[k] = _extraTime + ((int)k - 'A') + 1;
-			}
-
-			std::vector<char> workerTasks(_numWorkers, '?');
-			while (!availablePart2.empty())
-			{
-				for (std::size_t i = 0; i < _numWorkers; ++i)
-				{
-					auto& currentTask = workerTasks[i];
-					if (currentTask == '?')
-					{
-						for (auto& [k, canBeAssigned] : availablePart2)
-						{
-							if (canBeAssigned)
-							{
-								canBeAssigned = false;
-								currentTask = k;
-								break;
-							}
-						}
-					}
-					if (currentTask != '?')
-					{
-						auto& work = remainingWork[currentTask];
-						work--;
-						if (work <= 0)
-						{
-							done += currentTask;
-							availablePart2.erase(currentTask);
-
-							for (auto& [k, set] : reverseRulesPart2)
-							{
-								if (!set.empty())
-								{
-									set.erase(currentTask);
-									if (set.empty())
-									{
-										availablePart2[k] = true;
-									}
-								}
-							}
-
-							currentTask = '?';
-							for (auto& [k, canBeAssigned] : availablePart2)
-							{
-								if (canBeAssigned)
-								{
-									canBeAssigned = false;
-									currentTask = k;
-									break;
-								}
-							}
-						}
-					}
-				}
-
-				std::cout << time << "\t";
-				for (const auto t : workerTasks)
-				{
-					std::cout << t << "\t";
-				}
-				std::cout << done << std::endl;
-
-				time++;
-			}
-		}
-
-
-		return { part1, std::to_string(time) };
+		return { part1.first, std::to_string(part2.second) };
 	}
 }
