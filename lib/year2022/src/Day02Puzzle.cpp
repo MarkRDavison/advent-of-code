a7ba7fae116bb6a10c3434754fd64367392086a5
mark.davison <mark.davison@taxlab.online>
2022-12-02 19:26:06 +1300

    2022 day 2

--- lib/year2022/src/Day02Puzzle.cpp
+++ lib/year2022/src/Day02Puzzle.cpp
@@ -1,10 +1,24 @@
 #include <2022/Day02Puzzle.hpp>
 #include <zeno-engine/Utility/StringExtensions.hpp>
+#include <unordered_map>
+
+#define ROCK_THEM 'A'
+#define PAPER_THEM 'B'
+#define SCISSORS_THEM 'C'
+
+#define ROCK_YOU 'X'
+#define PAPER_YOU 'Y'
+#define SCISSORS_YOU 'Z'
+
+#define RESULT_LOSE 'X'
+#define RESULT_DRAW 'Y'
+#define RESULT_WIN 'Z'
+
 
 namespace TwentyTwentyTwo {
 	
 	Day02Puzzle::Day02Puzzle() :
-		core::PuzzleBase("Untitled Puzzle", 2022, 2) {
+		core::PuzzleBase("Rock Paper Scissors", 2022, 2) {
 	}
 
 
@@ -16,7 +30,70 @@ namespace TwentyTwentyTwo {
 		m_InputLines = std::vector<std::string>(_inputLines);
 	}
 
+	int scoreRound(char _them, char _you)
+	{
+		int roundScore = (int)(_you - ROCK_YOU) + 1;
+		int result = 0;
+
+		if (
+			_them == ROCK_THEM && _you == ROCK_YOU ||
+			_them == PAPER_THEM && _you == PAPER_YOU ||
+			_them == SCISSORS_THEM && _you == SCISSORS_YOU
+		)
+		{
+			result = 3;
+		}
+		else if (
+			_them == ROCK_THEM && _you == PAPER_YOU ||
+			_them == PAPER_THEM && _you == SCISSORS_YOU ||
+			_them == SCISSORS_THEM && _you == ROCK_YOU)
+		{
+			result = 6;
+		}
+
+		return roundScore + result;
+	}
+
 	std::pair<std::string, std::string> Day02Puzzle::fastSolve() {
-		return { "Part 1", "Part 2" };
+
+
+		const std::unordered_map<char, std::unordered_map<char, char>> themOutcomeYouMap
+		{
+			{ ROCK_THEM,
+				{
+					{ RESULT_LOSE, SCISSORS_YOU },
+					{ RESULT_DRAW, ROCK_YOU },
+					{ RESULT_WIN,  PAPER_YOU }
+				}
+			},
+
+			{ PAPER_THEM,
+				{
+					{ RESULT_LOSE, ROCK_YOU },
+					{ RESULT_DRAW, PAPER_YOU },
+					{ RESULT_WIN, SCISSORS_YOU }
+				}
+			},
+
+			{ SCISSORS_THEM,
+				{
+					{ RESULT_LOSE, PAPER_YOU },
+					{ RESULT_DRAW, SCISSORS_YOU },
+					{ RESULT_WIN, ROCK_YOU }
+				}
+			}
+		};
+
+		int part1 = 0;
+		int part2 = 0;
+
+		for (const auto& l : m_InputLines)
+		{
+			const char you = themOutcomeYouMap.at(l[0]).at(l[2]);
+			part1 += scoreRound(l[0], l[2]);
+			part2 += scoreRound(l[0], you);
+		}
+
+		return { std::to_string(part1), std::to_string(part2) };
 	}
 }
--- test/2022/Day02PuzzleTests.cpp
+++ test/2022/Day02PuzzleTests.cpp
@@ -4,13 +4,20 @@
 namespace TwentyTwentyTwo {
 
 	TEST_CASE("Day 2 Examples work", "[2022][Day02]") {
-	    const std::vector<std::string> input = {};
+	    const std::vector<std::string> input = {
+"A Y",
+"B X",
+"C Z"
+		};
 
 	    Day02Puzzle puzzle{};
 	    puzzle.setVerbose(true);
 	    puzzle.setInputLines(input);
 
 	    auto answers = puzzle.fastSolve();
+
+		REQUIRE("15" == answers.first);
+		REQUIRE("12" == answers.second);
 	}
 
 }
