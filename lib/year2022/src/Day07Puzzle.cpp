dcb0c072079f4a4dfa03fe51f8761ba363e3451c
mark.davison <mark.davison@taxlab.online>
2022-12-07 19:23:49 +1300

    2022 day 7

--- lib/year2022/src/Day07Puzzle.cpp
+++ lib/year2022/src/Day07Puzzle.cpp
@@ -1,10 +1,12 @@
 #include <2022/Day07Puzzle.hpp>
 #include <zeno-engine/Utility/StringExtensions.hpp>
+#include <cassert>
+#include <unordered_map>
 
 namespace TwentyTwentyTwo {
 	
 	Day07Puzzle::Day07Puzzle() :
-		core::PuzzleBase("Untitled Puzzle", 2022, 7) {
+		core::PuzzleBase("No Space Left On Device", 2022, 7) {
 	}
 
 
@@ -16,7 +18,148 @@ namespace TwentyTwentyTwo {
 		m_InputLines = std::vector<std::string>(_inputLines);
 	}
 
+	struct Directory {
+		std::string name;
+		std::vector<Directory *> children;
+		Directory* parent{ nullptr };
+
+		long size{ 0 };
+		long childrenSize{ 0 };
+	};
+
+	void updateFileSizes(Directory* _current) 
+	{
+		for (auto d : _current->children)
+		{
+			updateFileSizes(d);
+			_current->childrenSize += d->size + d->childrenSize;
+		}
+	}
+
+	void identifyBelowThreshold(Directory* _current, std::vector<Directory*>& _identified)
+	{
+		for (auto d : _current->children)
+		{
+			identifyBelowThreshold(d, _identified);
+		}
+
+		if (_current->size + _current->childrenSize <= 100000)
+		{
+			_identified.push_back(_current);
+		}
+	}
+
+	long solvePart1(Directory* _root)
+	{
+		std::vector<Directory*> identified;
+		identifyBelowThreshold(_root, identified);
+
+		long answer = 0;
+
+		for (const auto dir : identified)
+		{
+			auto size = dir->size + dir->childrenSize;
+			answer += size;
+		}
+
+		return answer;
+	}
+
+	void fetchSizes(Directory* _current, std::unordered_map<std::string, long>& _sizes)
+	{
+		_sizes[_current->name] = _current->size + _current->childrenSize;
+		for (auto d : _current->children)
+		{
+			fetchSizes(d, _sizes);
+		}
+	}
+
+	long solvePart2(Directory* _root)
+	{
+		const long TotalSize = 70000000;
+		const long FreeSpaceNeeded = 30000000;
+
+		const long usedSpace = _root->size + _root->childrenSize;
+		const long unusedSpace = TotalSize - usedSpace;
+
+		const long spaceToFree = FreeSpaceNeeded - unusedSpace;
+
+		std::unordered_map<std::string, long> dirSizes;
+		fetchSizes(_root, dirSizes);
+
+		long min = std::numeric_limits<long>::max();
+
+		for (const auto& [name, size] : dirSizes)
+		{
+			if (size >= spaceToFree && min > size)
+			{
+				min = size;
+			}
+		}
+
+		return min;
+	}
+
 	std::pair<std::string, std::string> Day07Puzzle::fastSolve() {
-		return { "Part 1", "Part 2" };
+		assert("$ cd /" == m_InputLines[0]);
+
+		Directory* root = new Directory();
+		root->name = "/";
+
+		Directory* current = root;
+
+		std::vector<std::string> location{ "/" };
+
+		bool first = true;
+		for (const auto& l : m_InputLines)
+		{
+			if (first) {
+				first = false;
+				continue;
+			}
+
+			if (ze::StringExtensions::startsWith(l, "$"))
+			{
+				if (ze::StringExtensions::startsWith(l, "$ cd ")) 
+				{
+					const auto& p = ze::StringExtensions::splitStringByDelimeter(l, " ");
+					const auto newDirectory = p[2];
+
+					if (newDirectory == "..")
+					{
+						current = current->parent;
+					}
+					else
+					{
+						location.push_back(p[2]);
+						auto dir = new Directory();
+						dir->name = newDirectory;
+						current->children.push_back(dir);
+						dir->parent = current;
+						current = dir;
+					}
+				}
+			}
+			else if (ze::StringExtensions::startsWith(l, "dir "))
+			{
+				// Directory				
+			}
+			else
+			{
+				// File
+				const auto& p = ze::StringExtensions::splitStringByDelimeter(l, " ");
+				const auto size = std::stol(p[0]);
+				const auto filename = p[1];
+
+				current->size += size;
+			}
+		}
+
+		updateFileSizes(root);
+
+		const auto part1 = solvePart1(root);
+		const auto part2 = solvePart2(root);
+
+		return { std::to_string(part1), std::to_string(part2) };
 	}
 }
--- test/2022/Day07PuzzleTests.cpp
+++ test/2022/Day07PuzzleTests.cpp
@@ -4,13 +4,40 @@
 namespace TwentyTwentyTwo {
 
 	TEST_CASE("Day 7 Examples work", "[2022][Day07]") {
-	    const std::vector<std::string> input = {};
+	    const std::vector<std::string> input = {
+"$ cd /",
+"$ ls",
+"dir a",
+"14848514 b.txt",
+"8504156 c.dat",
+"dir d",
+"$ cd a",
+"$ ls",
+"dir e",
+"29116 f",
+"2557 g",
+"62596 h.lst",
+"$ cd e",
+"$ ls",
+"584 i",
+"$ cd ..",
+"$ cd ..",
+"$ cd d",
+"$ ls",
+"4060174 j",
+"8033020 d.log",
+"5626152 d.ext",
+"7214296 k",
+		};
 
 	    Day07Puzzle puzzle{};
 	    puzzle.setVerbose(true);
 	    puzzle.setInputLines(input);
 
 	    auto answers = puzzle.fastSolve();
+
+		REQUIRE("95437" == answers.first);
+		REQUIRE("24933642" == answers.second);
 	}
 
 }
