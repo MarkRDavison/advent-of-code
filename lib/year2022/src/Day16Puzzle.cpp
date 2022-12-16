4fbf51968a81455f2d8fbb37362f231f0cadcf4d
mark.davison <mark.davison@taxlab.online>
2022-12-16 21:48:31 +1300

    2022 day 16

--- lib/year2022/src/Day16Puzzle.cpp
+++ lib/year2022/src/Day16Puzzle.cpp
@@ -1,10 +1,16 @@
 #include <2022/Day16Puzzle.hpp>
 #include <zeno-engine/Utility/StringExtensions.hpp>
+#include <unordered_map>
+#include <unordered_set>
+#include <functional>
+
+#define UNSET -1
 
 namespace TwentyTwentyTwo {
-	
+
+
 	Day16Puzzle::Day16Puzzle() :
-		core::PuzzleBase("Untitled Puzzle", 2022, 16) {
+		core::PuzzleBase("Proboscidea Volcanium", 2022, 16) {
 	}
 
 
@@ -16,7 +22,97 @@ namespace TwentyTwentyTwo {
 		m_InputLines = std::vector<std::string>(_inputLines);
 	}
 
+	using Memoized = std::unordered_map<std::string, std::unordered_map<long, std::unordered_map<long, long>>>;
+
 	std::pair<std::string, std::string> Day16Puzzle::fastSolve() {
-		return { "Part 1", "Part 2" };
+		std::unordered_map<std::string, int> flowRates;
+		std::unordered_map<std::string, std::vector<std::string>> adjacentValves;
+
+		std::vector<std::string> valves;
+		std::unordered_map<std::string, int> valveIds;
+
+		const std::string start = "AA";
+
+		int i = 0;
+		for (const auto& l : m_InputLines)
+		{
+			const auto& p = ze::StringExtensions::splitStringByDelimeter(l, " =;,");
+
+			const auto valve = p[1];
+			const auto flow = std::stoi(p[5]);
+
+			if (flow > 0)
+			{
+				valveIds[valve] = i;
+				i++;
+			}
+
+			flowRates[valve] = flow;
+			
+			valves.push_back(valve);
+
+			for (std::size_t i = 10; i < p.size(); ++i) {
+				const auto next = p[i];
+				adjacentValves[valve].push_back(next);
+			}
+		}
+				
+		Memoized cache;
+
+		const long maxPossibleStates = (long)std::powl(2, valveIds.size()) - 1;
+
+		long best = 0;
+
+		auto getCache = [](Memoized& _cache, std::string _valve, long _timeRemaining, long _possibleIndex) -> long& {
+			auto iter = _cache[_valve][_timeRemaining].find(_possibleIndex);
+
+			if (iter == _cache[_valve][_timeRemaining].end())
+			{
+				_cache[_valve][_timeRemaining][_possibleIndex] = UNSET;
+			}
+
+			return _cache[_valve][_timeRemaining][_possibleIndex];
+		};
+
+		std::function<long(std::string, long, long)> recurser =
+			[&](std::string _valve, long _timeRemaining, long _possibleIndex) -> long
+		{
+			if (_timeRemaining == 0)
+			{
+				return 0;
+			}
+
+			if (getCache(cache, _valve, _timeRemaining, _possibleIndex) == UNSET)
+			{
+				long score = 0;
+				for (const auto& adjacent : adjacentValves[_valve])
+				{
+					auto score = recurser(adjacent, _timeRemaining - 1, _possibleIndex);
+					score = std::max(score, score);
+				}
+				auto possible = 1 << valveIds[_valve];
+				if (possible & _possibleIndex)
+				{
+					score = std::max(score, recurser(_valve, _timeRemaining - 1, _possibleIndex - possible) + flowRates[_valve] * (_timeRemaining - 1));
+				}
+				cache[_valve][_timeRemaining][_possibleIndex] = score;
+			}
+
+			return getCache(cache, _valve, _timeRemaining, _possibleIndex);
+		};
+
+		auto part1 = recurser(start, 30, maxPossibleStates);
+		long part2 = 0;
+
+		for (long i = 0; i <= maxPossibleStates; ++i)
+		{
+			const auto you = recurser(start, 26, maxPossibleStates - i);
+			const auto elephant = recurser(start, 26, i);
+
+			part2 = std::max(part2, you + elephant);
+		}
+
+
+		return { std::to_string(part1), std::to_string(part2) };
 	}
 }
--- test/2022/Day16PuzzleTests.cpp
+++ test/2022/Day16PuzzleTests.cpp
@@ -4,13 +4,27 @@
 namespace TwentyTwentyTwo {
 
 	TEST_CASE("Day 16 Examples work", "[2022][Day16]") {
-	    const std::vector<std::string> input = {};
+	    const std::vector<std::string> input = {
+"Valve AA has flow rate=0; tunnels lead to valves DD, II, BB",
+"Valve BB has flow rate=13; tunnels lead to valves CC, AA",
+"Valve CC has flow rate=2; tunnels lead to valves DD, BB",
+"Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE",
+"Valve EE has flow rate=3; tunnels lead to valves FF, DD",
+"Valve FF has flow rate=0; tunnels lead to valves EE, GG",
+"Valve GG has flow rate=0; tunnels lead to valves FF, HH",
+"Valve HH has flow rate=22; tunnel leads to valve GG",
+"Valve II has flow rate=0; tunnels lead to valves AA, JJ",
+"Valve JJ has flow rate=21; tunnel leads to valve II",
+		};
 
 	    Day16Puzzle puzzle{};
 	    puzzle.setVerbose(true);
 	    puzzle.setInputLines(input);
 
 	    auto answers = puzzle.fastSolve();
+
+		REQUIRE("1651" == answers.first);
+		REQUIRE("1707" == answers.second);
 	}
 
 }
