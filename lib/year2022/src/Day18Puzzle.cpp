9df43a8dc86b834b82b5324a71312323ecc8a32c
mark.davison <mark.davison@taxlab.online>
2022-12-18 18:16:00 +1300

    2022 day 18 p1

--- lib/year2022/src/Day18Puzzle.cpp
+++ lib/year2022/src/Day18Puzzle.cpp
@@ -1,10 +1,28 @@
 #include <2022/Day18Puzzle.hpp>
 #include <zeno-engine/Utility/StringExtensions.hpp>
+#include <zeno-engine/Core/Vector4.hpp>
+#include <cassert>
+#include <unordered_set>
+#include <unordered_map>
 
 namespace TwentyTwentyTwo {
+
+	template<typename T>
+	struct vector3_hash_fxn_TODO_CORE_ME3 {
+		std::size_t operator()(const ze::Vector3<T>& _vec) const {
+			return  std::hash<T>()(_vec.x) ^ std::hash<T>()(_vec.y) ^ std::hash<T>()(_vec.z);
+		}
+	};
+
+	template<typename T>
+	struct vector4_hash_fxn_TODO_CORE_ME3 {
+		std::size_t operator()(const ze::Vector4<T>& _vec) const {
+			return  std::hash<T>()(_vec.x) ^ std::hash<T>()(_vec.y) ^ std::hash<T>()(_vec.z) ^ std::hash<T>()(_vec.w);
+		}
+	};
 	
 	Day18Puzzle::Day18Puzzle() :
-		core::PuzzleBase("Untitled Puzzle", 2022, 18) {
+		core::PuzzleBase("Boiling Boulders", 2022, 18) {
 	}
 
 
@@ -17,6 +35,75 @@ namespace TwentyTwentyTwo {
 	}
 
 	std::pair<std::string, std::string> Day18Puzzle::fastSolve() {
-		return { "Part 1", "Part 2" };
+
+		std::unordered_map<std::string, int> faces;
+
+		for (const auto& l : m_InputLines)
+		{
+			const auto& p = ze::StringExtensions::splitStringByDelimeter(l, ",");
+
+			ze::Vector3i coord = {
+				std::stoi(p[0]),
+				std::stoi(p[1]),
+				std::stoi(p[2])
+			};
+
+			const auto left =
+				std::to_string(coord.x-1) + "-" +
+				std::to_string(coord.y) + "-" +
+				std::to_string(coord.z) + "-x";
+
+			const auto right =
+				std::to_string(coord.x) + "-" +
+				std::to_string(coord.y) + "-" +
+				std::to_string(coord.z) + "-x";
+
+			const auto up =
+				std::to_string(coord.x) + "-" +
+				std::to_string(coord.y-1) + "-" +
+				std::to_string(coord.z) + "-y";
+
+			const auto down =
+				std::to_string(coord.x) + "-" +
+				std::to_string(coord.y) + "-" +
+				std::to_string(coord.z) + "-y";
+
+			const auto in =
+				std::to_string(coord.x) + "-" +
+				std::to_string(coord.y) + "-" +
+				std::to_string(coord.z-1) + "-z";
+
+			const auto out =
+				std::to_string(coord.x) + "-" +
+				std::to_string(coord.y) + "-" +
+				std::to_string(coord.z) + "-z";
+
+			std::cout << l
+				<< "\n\tleft: " << left
+				<< "\n\tright: " << right
+				<< "\n\tup: " << up
+				<< "\n\tdown: " << down
+				<< "\n\tin: " << in
+				<< "\n\tout: " << out
+				<< std::endl;
+
+			faces[left]++;
+			faces[right]++;
+			faces[up]++;
+			faces[down]++;
+			faces[in]++;
+			faces[out]++;
+		}
+
+		int part1 = 0;
+		for (const auto& f : faces)
+		{
+			if (f.second == 1)
+			{
+				part1++;
+			}
+		}
+
+		return { std::to_string(part1), "Part 2"};
 	}
 }
--- test/2022/Day18PuzzleTests.cpp
+++ test/2022/Day18PuzzleTests.cpp
@@ -4,13 +4,29 @@
 namespace TwentyTwentyTwo {
 
 	TEST_CASE("Day 18 Examples work", "[2022][Day18]") {
-	    const std::vector<std::string> input = {};
+	    const std::vector<std::string> input = {
+"2,2,2",
+"1,2,2",
+"3,2,2",
+"2,1,2",
+"2,3,2",
+"2,2,1",
+"2,2,3",
+"2,2,4",
+"2,2,6",
+"1,2,5",
+"3,2,5",
+"2,1,5",
+"2,3,5",
+		};
 
 	    Day18Puzzle puzzle{};
 	    puzzle.setVerbose(true);
 	    puzzle.setInputLines(input);
 
 	    auto answers = puzzle.fastSolve();
+
+		REQUIRE("64" == answers.first);
 	}
 
 }
