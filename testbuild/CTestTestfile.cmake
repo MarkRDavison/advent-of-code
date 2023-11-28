bf6ad95e8a55e4aa9632ef34891620be1178db01
mark.davison <markdavison0+github@gmail.com>
2023-11-28 16:47:06 +1300

    remove

--- testbuild/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: F:/Workspace/Git/advent-of-code
-# Build directory: F:/Workspace/Git/advent-of-code/testbuild
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lib")
-subdirs("app")
-subdirs("test")
