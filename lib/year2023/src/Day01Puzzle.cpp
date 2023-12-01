4807d29dc4d8aa9abfa9aaa9b25a3982f92a191a
mark.davison <markdavison0+github@gmail.com>
2023-12-01 18:40:37 +1300

    2023 day 1

--- lib/year2023/src/Day01Puzzle.cpp
+++ lib/year2023/src/Day01Puzzle.cpp
@@ -1,10 +1,13 @@
 #include <2023/Day01Puzzle.hpp>
 #include <Core/StringExtensions.hpp>
+#include <string>
+#include <numeric>
+#include <cassert>
 
 namespace TwentyTwentyThree {
 	
 	Day01Puzzle::Day01Puzzle() :
-		core::PuzzleBase("Untitled Puzzle", 2023, 1) {
+		core::PuzzleBase("Trebuchet?!", 2023, 1) {
 	}
 
 
@@ -17,6 +20,76 @@ namespace TwentyTwentyThree {
 	}
 
 	std::pair<std::string, std::string> Day01Puzzle::fastSolve() {
-		return { "Part 1", "Part 2" };
+		long part1 = 0;
+		long part2 = 0;
+
+		const std::vector<std::string> numbers = {
+			"zero",
+			"one",
+			"two",
+			"three",
+			"four",
+			"five",
+			"six",
+			"seven",
+			"eight",
+			"nine"
+		};
+
+		for (const auto& l : m_InputLines)
+		{
+			const auto firstNumeric = l.find_first_of("0123456789");
+			const auto lastNumeric = l.find_last_of("0123456789");
+
+			{
+				if (firstNumeric != std::numeric_limits<std::size_t>::max() &&
+					lastNumeric != std::numeric_limits<std::size_t>::max())
+				{
+					const auto firstDigit = (long)l[firstNumeric] - (long)'0';
+					const auto lastDigit = (long)l[lastNumeric] - (long)'0';
+
+					part1 += firstDigit * 10 + lastDigit;
+				}
+			}
+
+			std::vector<long> foundNumbers;
+			
+			for (std::size_t i = 0; i < l.size(); ++i)
+			{
+				const char c = l[i];
+				if ('0' <= c && c <= '9')
+				{
+					foundNumbers.push_back((long)(c - '0'));
+				}
+				else
+				{
+					long index = 0;
+					for (const auto& strNum : numbers)
+					{
+						const auto loc = l.find(strNum, i);
+						if (loc == i)
+						{
+							foundNumbers.push_back(index);
+							break;
+						}
+						index++;
+					}
+				}
+			}
+
+			if (getVerbose())
+			{
+				std::cout << l << '\n';
+				for (const auto& d : foundNumbers)
+				{
+					std::cout << " " << d;
+				}
+				std::cout << std::endl;
+			}
+
+			part2 += foundNumbers[0] * 10 + foundNumbers.back();
+		}
+
+		return { std::to_string(part1), std::to_string(part2) };
 	}
 }
--- test/2023/Day01PuzzleTests.cpp
+++ test/2023/Day01PuzzleTests.cpp
@@ -3,16 +3,59 @@
 
 namespace TwentyTwentyThree {
 
-	TEST_CASE("Day 1 Examples work", "[2023][Day01]") {
-	    const std::vector<std::string> input = {};
+	TEST_CASE("Day 1 Example part specific repeating works", "[2023][Day01]")
+	{
+		const std::vector<std::string> input = {
+		"one6gmpjfouronettbbjdqsxn"
+		};
 
-	    Day01Puzzle puzzle{};
-	    puzzle.setVerbose(true);
-	    puzzle.setInputLines(input);
+		Day01Puzzle puzzle{};
+		puzzle.setVerbose(true);
+		puzzle.setInputLines(input);
 
-	    auto answers = puzzle.fastSolve();
+		auto answers = puzzle.fastSolve();
 
-		REQUIRE("part1" == answers.first);
+ 		REQUIRE("66" == answers.first);
+		REQUIRE("11" == answers.second);
+	}
+
+	TEST_CASE("Day 1 Example part1 work", "[2023][Day01]")
+	{
+		const std::vector<std::string> input = {
+"1abc2",
+"pqr3stu8vwx",
+"a1b2c3d4e5f",
+"treb7uchet",
+		};
+
+		Day01Puzzle puzzle{};
+		puzzle.setVerbose(true);
+		puzzle.setInputLines(input);
+
+		auto answers = puzzle.fastSolve();
+
+		REQUIRE("142" == answers.first);
+	}
+
+	TEST_CASE("Day 1 Example part2 work", "[2023][Day01]")
+	{
+		const std::vector<std::string> input = {
+"two1nine",
+"eightwothree",
+"abcone2threexyz",
+"xtwone3four",
+"4nineeightseven2",
+"zoneight234",
+"7pqrstsixteen",
+		};
+
+		Day01Puzzle puzzle{};
+		puzzle.setVerbose(true);
+		puzzle.setInputLines(input);
+
+		auto answers = puzzle.fastSolve();
+
+		REQUIRE("281" == answers.second);
 	}
 
 }
