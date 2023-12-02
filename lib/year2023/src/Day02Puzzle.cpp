9048b344fcf20a85e3c4775ad49f027705ad1d6c
mark.davison <markdavison0+github@gmail.com>
2023-12-02 18:21:37 +1300

    2023 day 2

--- lib/year2023/src/Day02Puzzle.cpp
+++ lib/year2023/src/Day02Puzzle.cpp
@@ -1,10 +1,12 @@
 #include <2023/Day02Puzzle.hpp>
 #include <Core/StringExtensions.hpp>
+#include <cassert>
+#include <unordered_map>
 
 namespace TwentyTwentyThree {
 	
 	Day02Puzzle::Day02Puzzle() :
-		core::PuzzleBase("Untitled Puzzle", 2023, 2) {
+		core::PuzzleBase("Cube Conundrum", 2023, 2) {
 	}
 
 
@@ -16,7 +18,62 @@ namespace TwentyTwentyThree {
 		m_InputLines = std::vector<std::string>(_inputLines);
 	}
 
+	struct CubeConundrumGame
+	{
+
+	};
+
 	std::pair<std::string, std::string> Day02Puzzle::fastSolve() {
-		return { "Part 1", "Part 2" };
+		std::vector<CubeConundrumGame> games;
+
+		const std::unordered_map<std::string, long long> contents = {
+			{ "red", 12 }, { "green", 13 }, { "blue", 14 }
+		};
+
+
+		long long part1 = 0;
+		long long part2 = 0;
+		long long gameNumber = 0;
+
+		for (const auto& l : m_InputLines)
+		{
+			gameNumber++;
+			const auto& parts = StringExtensions::splitStringByDelimeter(l, ":;");
+
+			bool possible = true;
+
+			std::unordered_map<std::string, long long> minContents = {
+				{ "red", 0 }, { "green", 0 }, { "blue", 0 }
+			};
+
+			for (std::size_t i = 1; i < parts.size(); ++i)
+			{
+				const auto& roundParts = StringExtensions::splitStringByDelimeter(parts[i], ", ");
+
+				assert(roundParts.size() % 2 == 0);
+
+				for (std::size_t j = 0; j < roundParts.size(); j += 2)
+				{
+					const auto& amount = std::stoll(roundParts[j + 0]);
+					const auto& type = roundParts[j + 1];
+
+					minContents[type] = std::max(minContents[type], amount);
+
+					if (contents.at(type) < amount)
+					{
+						possible = false;
+					}
+				}
+			}
+
+			if (possible)
+			{
+				part1 += gameNumber;
+			}
+
+			part2 += minContents.at("red") * minContents.at("green") * minContents.at("blue");
+		}
+
+		return { std::to_string(part1), std::to_string(part2) };
 	}
 }
