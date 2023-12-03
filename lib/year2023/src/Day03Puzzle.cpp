1d13254c3d948679ba679cca93b0b2fc45c0ba02
mark.davison <markdavison0+github@gmail.com>
2023-12-03 18:54:56 +1300

    2023 day 3 - so bad. needs cleaning

--- lib/year2023/src/Day03Puzzle.cpp
+++ lib/year2023/src/Day03Puzzle.cpp
@@ -1,10 +1,13 @@
 #include <2023/Day03Puzzle.hpp>
 #include <Core/StringExtensions.hpp>
+#include <Core/Region.hpp>
+#include <unordered_map>
+#include <unordered_set>
 
 namespace TwentyTwentyThree {
 	
 	Day03Puzzle::Day03Puzzle() :
-		core::PuzzleBase("Untitled Puzzle", 2023, 3) {
+		core::PuzzleBase("Gear Ratios", 2023, 3) {
 	}
 
 
@@ -16,7 +19,160 @@ namespace TwentyTwentyThree {
 		m_InputLines = std::vector<std::string>(_inputLines);
 	}
 
+	struct SchematicNumber
+	{
+		bool adj{ false };
+		int number{ 0 };
+	};
+
+	struct SchematicCell
+	{
+		char c{'.'};
+		int number;
+		bool adjacentSymbol;
+		SchematicNumber* sn{ nullptr };
+
+		bool isMagicSymbol()
+		{
+			auto res = c != '.' && c != '0' && c != '1' && c != '2' && c != '3' && c != '4' && c != '5' && c != '6' && c != '7' && c != '8' && c != '9';
+			return res;
+		}
+	};
+
 	std::pair<std::string, std::string> Day03Puzzle::fastSolve() {
-		return { "Part 1", "Part 2" };
+
+		core::Region<SchematicCell> schematic;
+
+		std::vector< SchematicNumber*> sns;
+
+		std::vector<std::unordered_set<SchematicNumber*>> gearNumbers;
+
+		for (int y = 0; y < m_InputLines.size(); ++y)
+		{
+			int currentNumber = -1;
+			SchematicNumber* currsn{ nullptr };
+			for (int x = 0; x < m_InputLines[y].size(); ++x)
+			{
+				auto& cell = schematic.getCell(x, y);
+				cell.c = m_InputLines[y][x];
+
+				if ('0' <= cell.c && cell.c <= '9')
+				{
+					if (currentNumber == -1)
+					{
+						auto end = m_InputLines[y].find_first_not_of("0123456789", x);
+						auto number = m_InputLines[y].substr(x, end - x);
+
+						currentNumber = std::stoi(number);
+						currsn = sns.emplace_back(new SchematicNumber());
+						currsn->adj = false;
+						currsn->number = currentNumber;
+					}
+					cell.number = currentNumber;
+					cell.sn = currsn;
+				}
+
+				if ('.' == cell.c || cell.c > '9' || cell.c < '0')
+				{
+					if (currentNumber != -1)
+					{
+						currentNumber = -1;
+					}
+				}
+			}
+		}
+
+		for (int y = 0; y < m_InputLines.size(); ++y)
+		{
+			for (int x= 0; x < m_InputLines[y].size(); ++x)
+			{
+				auto& cell = schematic.getCell(x, y);
+				if (cell.isMagicSymbol())
+				{
+
+					bool isGear = cell.c == '*';
+					auto& newGearVals =
+						gearNumbers.emplace_back();
+
+					int touching = 0;
+
+					for (int yOff = -1; yOff <= +1; ++yOff)
+					{
+						for (int xOff = -1; xOff <= +1; ++xOff)
+						{
+							if (xOff == 0 && yOff == 0) { continue; }
+
+							auto& adjcell = schematic.getCell(x + xOff, y + yOff);
+							if ('0' <= adjcell.c && adjcell.c <= '9')
+							{
+								adjcell.adjacentSymbol = true;
+								adjcell.sn->adj = true;
+								touching++;
+								if (isGear)
+								{
+									newGearVals.emplace(adjcell.sn);
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+
+		long long part1 = 0;
+		bool startTalking = false;
+
+		for (int y = 0; y < m_InputLines.size(); ++y)
+		{
+			for (int x = 0; x < m_InputLines[y].size(); ++x)
+			{
+				auto& cell = schematic.getCell(x, y);
+				if (cell.number != -1 && cell.adjacentSymbol)
+				{
+					part1 += cell.number;
+
+					int off = 0;
+					while (true)
+					{
+						const auto cc = schematic.getCell(x + 1 + off, y).c;
+						if (!('0' <= cc && cc <= '9'))
+						{
+							break;
+						}
+
+						off++;
+					}
+
+					x += off;
+				}
+			}
+		}
+
+		long long part1b = 0;
+
+		for (const auto& sn : sns)
+		{
+			if (sn->adj)
+			{
+				part1b += sn->number;
+			}
+		}
+
+		long long part2 = 0;
+
+		for (const auto& gv : gearNumbers)
+		{
+			if (gv.size() == 2)
+			{
+				long long p2c = 1;
+				for (const auto& sn : gv)
+				{
+					p2c *= sn->number;
+				}
+				part2 += p2c;
+			}
+		}
+
+		return { std::to_string(part1), std::to_string(part2)};
 	}
 }
--- test/2023/Day03PuzzleTests.cpp
+++ test/2023/Day03PuzzleTests.cpp
@@ -4,13 +4,27 @@
 namespace TwentyTwentyThree {
 
 	TEST_CASE("Day 3 Examples work", "[2023][Day03]") {
-	    const std::vector<std::string> input = {};
+	    const std::vector<std::string> input = {
+"467..114..",
+"...*......",
+"..35..633.",
+"......#...",
+"617*......",
+".....+.58.",
+"..592.....",
+"......755.",
+"...$.*....",
+".664.598..",
+		};
 
 	    Day03Puzzle puzzle{};
 	    puzzle.setVerbose(true);
 	    puzzle.setInputLines(input);
 
 	    auto answers = puzzle.fastSolve();
+
+		REQUIRE("4361" == answers.first);
+		REQUIRE("467835" == answers.second);
 	}
 
 }
