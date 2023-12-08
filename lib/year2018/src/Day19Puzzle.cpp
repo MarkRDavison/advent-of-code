555363cc71ea075d2d19be4c259710962ee6aba9
mark.davison <markdavison0+github@gmail.com>
2023-12-08 14:29:55 +1300

    2018 day 19

--- lib/year2018/src/Day19Puzzle.cpp
+++ lib/year2018/src/Day19Puzzle.cpp
@@ -1,10 +1,11 @@
 #include <2018/Day19Puzzle.hpp>
 #include <Core/StringExtensions.hpp>
+#include <2018/OpCodeComputer.hpp>
 
 namespace TwentyEighteen {
 	
 	Day19Puzzle::Day19Puzzle() :
-		core::PuzzleBase("Untitled Puzzle", 2018, 19) {
+		core::PuzzleBase("Go With The Flow", 2018, 19) {
 
 	}
 	Day19Puzzle::~Day19Puzzle() {
@@ -21,6 +22,90 @@ namespace TwentyEighteen {
 	}
 
 	std::pair<std::string, std::string> Day19Puzzle::fastSolve() {
-		return { "Part 1", "Part 2" };
+		const OpCodeNumber pcRegisterIndex = (OpCodeNumber)std::stoll(StringExtensions::splitStringByDelimeter(m_InputLines[0], " ")[1]);
+
+		std::vector<OpCode> instructions;
+
+		const std::unordered_map<std::string, OpCodeType> opcodes = {
+			{ "addr", OpCodeType::ADDR },
+			{ "addi", OpCodeType::ADDI },
+			{ "mulr", OpCodeType::MULR },
+			{ "muli", OpCodeType::MULI },
+			{ "banr", OpCodeType::BANR },
+			{ "bani", OpCodeType::BANI },
+			{ "borr", OpCodeType::BORR },
+			{ "bori", OpCodeType::BORI },
+			{ "setr", OpCodeType::SETR },
+			{ "seti", OpCodeType::SETI },
+			{ "gtir", OpCodeType::GTIR },
+			{ "gtri", OpCodeType::GTRI },
+			{ "gtrr", OpCodeType::GTRR },
+			{ "eqir", OpCodeType::EQIR },
+			{ "eqri", OpCodeType::EQRI },
+			{ "eqrr", OpCodeType::EQRR }
+		};
+
+		OpCodeComputer<6> computer;
+		computer.registers = { 0,0,0,0,0,0 };
+		OpCodeNumber& pc = computer.registers[pcRegisterIndex];
+
+		for (std::size_t i = 1; i < m_InputLines.size(); ++i)
+		{
+			const auto& parts = StringExtensions::splitStringByDelimeter(m_InputLines[i], " ");
+
+			instructions.emplace_back(
+				OpCode{
+					.code = opcodes.at(parts[0]),
+					.A = (OpCodeNumber)std::stoll(parts[1]),
+					.B = (OpCodeNumber)std::stoll(parts[2]),
+					.C = (OpCodeNumber)std::stoll(parts[3])
+				});
+		}
+
+		while (0 <= pc && pc < (OpCodeNumber)instructions.size())
+		{
+			const auto& instr = instructions[pc];
+
+			computer.runOpCode(instr);
+
+			pc++;
+		}
+
+		OpCodeNumber part1 = computer.registers[0];
+		OpCodeNumber part2 = 0;
+
+		computer.registers = { 1,0,0,0,0,0 };
+
+		OpCodeNumber lastPc = pc;
+
+		while (0 <= pc && pc < (OpCodeNumber)instructions.size())
+		{
+			const auto& instr = instructions[pc];
+
+			computer.runOpCode(instr);
+
+			pc++;
+
+			if (pc > lastPc)
+			{
+				lastPc = pc;
+			}
+			else
+			{
+				break;
+			}
+		}
+
+		const auto largeNum = *std::max_element(computer.registers.begin(), computer.registers.end());
+			
+		for (OpCodeNumber x = 1; x <= largeNum; ++x)
+		{
+			if (largeNum % x == 0)
+			{
+				part2 += x;
+			}
+		}
+
+		return { std::to_string(part1), std::to_string(part2)};
 	}
 }
--- test/2018/Day19PuzzleTests.cpp
+++ test/2018/Day19PuzzleTests.cpp
@@ -3,24 +3,26 @@
 
 namespace TwentyEighteen {
 
-	TEST_CASE("2018 Day 19 Part 1 Example work", "[2018][Day19]") {
-	    const std::vector<std::string> input = {};
+	TEST_CASE("2018 Day 19 Examples work", "[2018][Day19]") {
+	    const std::vector<std::string> input = {
+"#ip 0",
+"seti 5 0 1",
+"seti 6 0 2",
+"addi 0 1 0",
+"addr 1 2 3",
+"setr 1 0 0",
+"seti 8 0 4",
+"seti 9 0 5",
+		};
 
 	    Day19Puzzle puzzle{};
 	    puzzle.setVerbose(true);
 	    puzzle.setInputLines(input);
 
 	    auto answers = puzzle.fastSolve();
-	}
 
-	TEST_CASE("2018 Day 19 Part 2 Example work", "[2018][Day19]") {
-	    const std::vector<std::string> input = {};
-
-	    Day19Puzzle puzzle{};
-	    puzzle.setVerbose(true);
-	    puzzle.setInputLines(input);
-
-	    auto answers = puzzle.fastSolve();
+		REQUIRE("7" == answers.first);
+		REQUIRE("12" == answers.second);
 	}
 
 }
