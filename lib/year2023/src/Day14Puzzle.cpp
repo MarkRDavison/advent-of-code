f1800bcb1e355d615b62e234925b226d11ccbf6b
mark.davison <markdavison0+github@gmail.com>
2023-12-14 18:35:41 +1300

    2023 day 14 - 356th!!!!!! woop!

--- lib/year2023/src/Day14Puzzle.cpp
+++ lib/year2023/src/Day14Puzzle.cpp
@@ -1,10 +1,17 @@
 #include <2023/Day14Puzzle.hpp>
 #include <Core/StringExtensions.hpp>
+#include <unordered_map>
+#include <numeric>
+#include <cassert>
+
+#define ROUND 'O'
+#define CUBE '#'
+#define EMPTY '.'
 
 namespace TwentyTwentyThree {
 	
 	Day14Puzzle::Day14Puzzle() :
-		core::PuzzleBase("Untitled Puzzle", 2023, 14) {
+		core::PuzzleBase("Parabolic Reflector Dish", 2023, 14) {
 	}
 
 
@@ -16,7 +23,204 @@ namespace TwentyTwentyThree {
 		m_InputLines = std::vector<std::string>(_inputLines);
 	}
 
+	typedef std::vector<std::string> Platform;
+
+	void runCycle(Platform& platform)
+	{
+		// Tilt NORTH
+		bool changeMade = true;
+		while (changeMade)
+		{
+			changeMade = false;
+
+			for (std::size_t x = 0; x < platform[0].size(); ++x)
+			{
+				for (std::size_t y = 0; y < platform.size() - 1; ++y)
+				{
+					auto& curr = platform[y][x];
+					auto& above = platform[y + 1][x];
+					if (curr == EMPTY && above == ROUND)
+					{
+						std::swap(curr, above);
+						changeMade = true;
+					}
+				}
+			}
+		}
+
+		// Tilt WEST
+		changeMade = true;
+		while (changeMade)
+		{
+			changeMade = false;
+
+			for (std::size_t y = 0; y < platform.size(); ++y)
+			{
+				for (std::size_t x = 0; x < platform[0].size() - 1; ++x)
+				{
+					auto& curr = platform[y][x];
+					auto& above = platform[y][x + 1];
+					if (curr == EMPTY && above == ROUND)
+					{
+						std::swap(curr, above);
+						changeMade = true;
+					}
+				}
+			}
+		}
+
+		// Tilt SOUTH
+		changeMade = true;
+		while (changeMade)
+		{
+			changeMade = false;
+
+			for (std::size_t x = 0; x < platform[0].size(); ++x)
+			{
+				for (std::size_t y = platform.size() - 1; y != 0; --y)
+				{
+					auto& curr = platform[y][x];
+					auto& above = platform[y - 1][x];
+					if (curr == EMPTY && above == ROUND)
+					{
+						std::swap(curr, above);
+						changeMade = true;
+					}
+				}
+			}
+		}
+
+		// Tilt EAST
+		changeMade = true;
+		while (changeMade)
+		{
+			changeMade = false;
+
+			for (std::size_t y = 0; y < platform.size(); ++y)
+			{
+				for (std::size_t x = platform[0].size() - 1; x != 0 ; --x)
+				{
+					auto& curr = platform[y][x];
+					auto& above = platform[y][x - 1];
+					if (curr == EMPTY && above == ROUND)
+					{
+						std::swap(curr, above);
+						changeMade = true;
+					}
+				}
+			}
+		}
+	}
+
+	std::string join(const Platform& platform)
+	{
+		return std::accumulate(
+			std::next(platform.begin()),
+			platform.end(),
+			platform[0],
+			[](const std::string& lhs, const std::string& rhs)
+			{
+				return lhs + rhs;
+			}
+		);
+	}
+
+	long long getTotalLoad(const Platform& platform)
+	{
+		long long totalLoad = 0;
+
+		for (std::size_t i = 0; i < platform.size(); ++i)
+		{
+			for (std::size_t x = 0; x < platform[0].size(); ++x)
+			{
+				if (platform[i][x] == ROUND)
+				{
+					totalLoad += platform.size() - i;
+				}
+			}
+		}
+
+		return totalLoad;
+	}
+
+	long long solvePart2(Platform platform)
+	{
+		std::unordered_map<std::string, std::size_t> platformToIndex;
+		std::unordered_map<std::size_t, long long> indexToLoad;
+
+		std::size_t i = 0;
+
+		std::size_t periodStart = 0;
+		std::size_t periodEnd = 0;
+
+		constexpr std::size_t targetI = 1'000'000'000;
+
+		while (true)
+		{
+			runCycle(platform); ++i;
+
+			const auto key = join(platform);
+
+			if (platformToIndex.contains(key))
+			{
+				periodStart = platformToIndex.at(key);
+				periodEnd = i;
+				break;
+			}
+			else
+			{
+				platformToIndex[key] = i;
+				indexToLoad[i] = getTotalLoad(platform);
+			}
+		}
+
+		const auto period = periodEnd - periodStart;
+		const auto cycle = targetI - periodStart;
+
+		assert(period > 0);
+		assert(cycle > 0);
+
+		const auto sameLoadIndex = periodStart + cycle % period;
+
+		assert(indexToLoad.contains(sameLoadIndex));
+
+		return indexToLoad[sameLoadIndex];
+	}
+
+	long long solvePart1(Platform platform)
+	{
+		std::size_t i = 1;
+
+		bool changeMade = true;
+		while (changeMade)
+		{
+			changeMade = false;
+
+			for (std::size_t x = 0; x < platform[0].size(); ++x)
+			{
+				for (std::size_t y = 0; y < platform.size() - 1; ++y)
+				{
+					auto& curr = platform[y][x];
+					auto& above = platform[y + 1][x];
+					if (curr == EMPTY && above == ROUND)
+					{
+						std::swap(curr, above);
+						changeMade = true;
+					}
+				}
+			}
+
+			++i;
+		}
+
+
+		return getTotalLoad(platform);
+	}
+
 	std::pair<std::string, std::string> Day14Puzzle::fastSolve() {
-		return { "Part 1", "Part 2" };
+		const auto part1 = solvePart1(m_InputLines);
+		const auto part2 = solvePart2(m_InputLines);
+
+		return { std::to_string(part1), std::to_string(part2) };
 	}
 }
--- test/2023/Day14PuzzleTests.cpp
+++ test/2023/Day14PuzzleTests.cpp
@@ -4,13 +4,27 @@
 namespace TwentyTwentyThree {
 
 	TEST_CASE("Day 14 Examples work", "[2023][Day14]") {
-	    const std::vector<std::string> input = {};
+	    const std::vector<std::string> input = {
+"O....#....",
+"O.OO#....#",
+".....##...",
+"OO.#O....O",
+".O.....O#.",
+"O.#..O.#.#",
+"..O..#O..O",
+".......O..",
+"#....###..",
+"#OO..#....",
+		};
 
 	    Day14Puzzle puzzle{};
 	    puzzle.setVerbose(true);
 	    puzzle.setInputLines(input);
 
 	    auto answers = puzzle.fastSolve();
+
+		REQUIRE("136" == answers.first);
+		REQUIRE("64" == answers.second);
 	}
 
 }
