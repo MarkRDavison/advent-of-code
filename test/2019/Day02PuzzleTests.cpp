325d29265aac5825d04ec2d39c8dc7e59d7c40d7
mark.davison <markdavison0+github@gmail.com>
2023-12-17 19:45:05 +1300

    works

--- test/2019/Day02PuzzleTests.cpp
+++ test/2019/Day02PuzzleTests.cpp
@@ -3,24 +3,17 @@
 
 namespace TwentyNineteen {
 
-	TEST_CASE("2019 Day 2 Part 1 Example work", "[2019][Day02]") {
-	    const std::vector<std::string> input = {};
+	TEST_CASE("2019 Day 2 Examples work", "[2019][Day02]") {
+	    const std::vector<std::string> input = { "1,9,10,3,2,3,11,0,99,30,40,50" };
 
 	    Day02Puzzle puzzle{};
 	    puzzle.setVerbose(true);
 	    puzzle.setInputLines(input);
 
-	    auto answers = puzzle.fastSolve();
-	}
+	    auto answers = puzzle.fastSolve(false);
 
-	TEST_CASE("2019 Day 2 Part 2 Example work", "[2019][Day02]") {
-	    const std::vector<std::string> input = {};
-
-	    Day02Puzzle puzzle{};
-	    puzzle.setVerbose(true);
-	    puzzle.setInputLines(input);
-
-	    auto answers = puzzle.fastSolve();
+		REQUIRE("3500" == answers.first);
+		REQUIRE("3500" == answers.second);
 	}
 
 }
