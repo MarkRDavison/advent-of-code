c56e5460633ca2626145aea8a409badaf340d492
mark.davison <markdavison0+github@gmail.com>
2023-12-23 01:28:08 +1300

    2019 day 3

--- lib/year2019/src/Day03Puzzle.cpp
+++ lib/year2019/src/Day03Puzzle.cpp
@@ -1,10 +1,13 @@
 #include <2019/Day03Puzzle.hpp>
 #include <Core/StringExtensions.hpp>
+#include <Core/Vector2.hpp>
+#include <unordered_map>
+#include <numeric>
 
 namespace TwentyNineteen {
 	
 	Day03Puzzle::Day03Puzzle() :
-		core::PuzzleBase("Untitled Puzzle", 2019, 3) {
+		core::PuzzleBase("Crossed Wires", 2019, 3) {
 
 	}
 	Day03Puzzle::~Day03Puzzle() {
@@ -21,6 +24,61 @@ namespace TwentyNineteen {
 	}
 
 	std::pair<std::string, std::string> Day03Puzzle::fastSolve() {
-		return { "Part 1", "Part 2" };
+		std::unordered_map<Vector2i, bool[2], Vector2_Hash_Fxn<int>> wirePaths;
+		std::unordered_map<Vector2i, int[2], Vector2_Hash_Fxn<int>> wireDistances;
+		const std::unordered_map<char, Vector2i> dirs = {
+			{'R', {+1,0}},
+			{'L', {-1,0}},
+			{'U', {0,-1}},
+			{'D', {0,+1}},
+		};
+
+		
+		const auto& runWire =
+			[&](const std::string& line, std::size_t index) -> void
+			{
+				Vector2i curr;
+				int distance = 0;
+				for (const auto& instr : StringExtensions::splitStringByDelimeter(line, ","))
+				{
+					const auto dir = instr[0];
+					const auto length = std::stoi(instr.substr(1));
+
+					for (int i = 0; i < length; ++i)
+					{
+						distance++;
+						curr += dirs.at(dir);
+						if (!wirePaths[curr][index])
+						{
+							wireDistances[curr][index] = distance;
+						}
+						wirePaths[curr][index] = true;
+					}
+				}
+			};
+
+		runWire(m_InputLines[0], 0);
+		runWire(m_InputLines[1], 1);
+
+		int part1 = std::numeric_limits<int>::max();
+		int part2 = std::numeric_limits<int>::max();
+		for (const auto& [coord, amount] : wirePaths)
+		{
+			if (amount[0] && amount[1])
+			{
+				const auto manhatten = std::abs(coord.x) + std::abs(coord.y);
+				if (part1 > manhatten)
+				{
+					part1 = manhatten;
+				}
+				const auto distance = wireDistances[coord][0] + wireDistances[coord][1];
+				if (part2 > distance)
+				{
+					part2 = distance;
+				}
+			}
+		}
+
+		return { std::to_string(part1), std::to_string(part2) };
 	}
 }
--- test/2019/Day03PuzzleTests.cpp
+++ test/2019/Day03PuzzleTests.cpp
@@ -3,24 +3,20 @@
 
 namespace TwentyNineteen {
 
-	TEST_CASE("2019 Day 3 Part 1 Example work", "[2019][Day03]") {
-	    const std::vector<std::string> input = {};
+	TEST_CASE("2019 Day 3 Examples work", "[2019][Day03]") {
+	    const std::vector<std::string> input = {
+"R8,U5,L5,D3",
+"U7,R6,D4,L4"
+		};
 
 	    Day03Puzzle puzzle{};
 	    puzzle.setVerbose(true);
 	    puzzle.setInputLines(input);
 
 	    auto answers = puzzle.fastSolve();
-	}
 
-	TEST_CASE("2019 Day 3 Part 2 Example work", "[2019][Day03]") {
-	    const std::vector<std::string> input = {};
-
-	    Day03Puzzle puzzle{};
-	    puzzle.setVerbose(true);
-	    puzzle.setInputLines(input);
-
-	    auto answers = puzzle.fastSolve();
+		REQUIRE("6" == answers.first);
+		REQUIRE("30" == answers.second);
 	}
 
 }
